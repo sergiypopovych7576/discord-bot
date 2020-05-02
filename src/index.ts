a81a155aa8e5a124189b7bbf93a110709f47bee3
Sergiy Popovych <popovychsergiy76@gmail.com>
2020-05-02 13:52:27 +0300

    Deploy Check

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ client.on('ready', async () => {
 client.on('message', async (msg) => {
     if(!msg.author.bot) {
         Logger.log(`Resieved message: ${msg.content}`);
-        msg.reply('Гав - 93');
+        msg.reply('Мяу');
     }
 });
 
