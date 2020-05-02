6ddf7a692765fd0a6536198822fd94aa8148940a
Sergiy Popovych <popovychsergiy76@gmail.com>
2020-05-02 15:13:04 +0300

    Fixed message filtering

--- src/filters/prefix.filter.ts
+++ src/filters/prefix.filter.ts
@@ -5,7 +5,7 @@ import config from '../settings.json';
 
 export class CommandPrefixFilter extends BaseFilter {
     public validate<K extends keyof ClientEvents>(...args: ClientEvents[K]): boolean {
-        if (typeof (args[0]) === typeof (Message)) {
+        if (args[0] instanceof Message) {
             const message = args[0] as Message;
 
             return message.content.charAt(0) === config.commandPrefix;
